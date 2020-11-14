1582514b23dbd4925cb14861338e125c6a05ea43
Laura <laura.a.chelaru@gmail.com>
2020-11-14 10:31:37 +0100

    aggiunto "inserire nome", creazione nuovo item e modifica

--- vue.js
+++ vue.js
@@ -1,6 +1,33 @@
 var app = new Vue({
     el: '#app',
     data: {
-      salute: 'Hello Vue!'
+        name : "",
+        newItem : "",
+        textEdited : "",
+        indexToEdit : "",
+        groceryList: [
+            {text : "Pane"},
+            {text : "Affettati"},
+            {text : "Formaggio"},
+            {text : "Verdura"},
+            {text : "Frutta"},
+            {text : "Fagioli in scatola"},
+            {text : "Gelato allo zabaione"}
+        ]
+    },
+    methods: {
+        addNew(item) {
+            var item = {text: item};
+            this.groceryList.push(item);
+            this.newItem = "";
+        },
+        clickToEdit(index) {
+            this.indexToEdit = index;
+            this.textEdited = this.groceryList[index].text;
+        },
+        confirmEdit(textEdited, indexToEdit) {
+            this.groceryList[indexToEdit].text = textEdited;
+            this.textEdited = "";
+        }
     }
   })
